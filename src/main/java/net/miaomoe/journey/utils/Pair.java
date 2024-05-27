22e5bcc963a326b5c4a23f20b02a7aafcfd0c588
Akkariin <fallencrystal@outlook.com>
2024-05-27 16:44:20 +0800

    It is now easier to create sets for pairs or convert them to maps.

--- src/main/java/net/miaomoe/journey/utils/Pair.java
+++ src/main/java/net/miaomoe/journey/utils/Pair.java
@@ -20,11 +20,16 @@ package net.miaomoe.journey.utils;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static net.miaomoe.journey.utils.Preconditions.checkNotNull;
 
 @Value
+@SuppressWarnings("unused")
 public class Pair<A, B> {
     A a;
     B b;
@@ -32,15 +37,60 @@ public class Pair<A, B> {
     public Optional<A> getOptionalA() { return Optional.ofNullable(a); }
     public Optional<B> getOptionalB() { return Optional.ofNullable(b); }
 
-    public void invoke(final @NotNull BiConsumer<A, B> consumer) {
-        Preconditions.checkNotNull(consumer, "consumer").accept(a, b);
+    public void apply(final @NotNull BiConsumer<A, B> consumer) {
+        checkNotNull(consumer, "consumer").accept(a, b);
     }
 
-    public <R> R invoke(final @NotNull BiFunction<A, B, R> function) {
-        return Preconditions.checkNotNull(function, "function").apply(a, b);
+    public <R> R let(final @NotNull BiFunction<A, B, R> function) {
+        return checkNotNull(function, "function").apply(a, b);
     }
+
+    public @NotNull Pair<A, B> copy() {
+        return new Pair<>(a, b);
+    }
+
+    public @NotNull Supplier<A> getSupplierA() { return () -> a; }
+    public @NotNull Supplier<B> getSupplierB() { return () -> b; }
 
     public static <A, B> Pair<A, B> of(final A a, final B b) {
         return new Pair<>(a, b);
+    }
+
+    public static <A, B, T> @NotNull Set<Pair<A, B>> toPairSet(
+            final @NotNull Collection<T> list,
+            final @NotNull Function<T, A> aProvider,
+            final @NotNull Function<T, B> bProvider
+    ) {
+        final HashSet<Pair<A, B>> pairs = new HashSet<>();
+        for (final T t : list) { pairs.add(Pair.of(aProvider.apply(t), bProvider.apply(t))); }
+        return pairs;
+    }
+
+    public static <K, V> @NotNull Map<K, V> toMap(final Collection<Pair<K, V>> pairs) {
+        return toMap(pairs, HashMap::new);
+    }
+
+    public static <K, V, M extends Map<K, V>> @NotNull M toMap(final Collection<Pair<K, V>> pairs, final @NotNull Supplier<M> supplier) {
+        final M map = checkNotNull(checkNotNull(supplier, "supplier").get(), "map");
+        for (final Pair<K, V> pair : checkNotNull(pairs, "pairs")) map.put(pair.a, pair.b);
+        return map;
+    }
+
+    public static <K, V> @NotNull Map<K, V> toMapReversed(final @NotNull Collection<Pair<V, K>> pairs) {
+        return toMapReversed(pairs, HashMap::new);
+    }
+
+    public static <K, V, M extends Map<K, V>> @NotNull Map<K, V> toMapReversed(final @NotNull Collection<Pair<V, K>> pairs, final @NotNull Supplier<M> supplier) {
+        final M map = checkNotNull(checkNotNull(supplier, "supplier").get(), "map");
+        for (final Pair<V, K> pair : checkNotNull(pairs, "pairs")) map.put(pair.b, pair.a);
+        return map;
+    }
+
+    public static <K, V> @NotNull Set<Pair<K, V>> fromMap(final @NotNull Map<K, V> map) {
+        return toPairSet(map.entrySet(), Map.Entry::getKey, Map.Entry::getValue);
+    }
+
+    public static <K, V> @NotNull Set<Pair<V, K>> fromMapReversed(final @NotNull Map<K, V> map) {
+        return toPairSet(map.entrySet(), Map.Entry::getValue, Map.Entry::getKey);
     }
 }
